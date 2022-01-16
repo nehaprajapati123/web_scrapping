454e1b41296aecb10b2721d17c018adbd4aecc73
nehaprajapati123 <91823695+nehaprajapati123@users.noreply.github.com>
2022-01-16 14:30:46 +0530

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,19 +1,22 @@
 import pprint
 import json
-from task5 import get_movie_list_details
-movie_list=get_movie_list_details()
-def analysis_movie_language():
+with open("task5.json","r+") as file:
+    movies_list=json.load(file)
+def analysis_movies_language():
     dic={}
-    for i in movie_list:
+    for i in movies_list:
         if "language" in i:
             a=i["language"]
             if a not in dic:
-                dic[a]=1
+                dic[a]=0
             else:
                 dic[a]+=1
         else:
             pass
+        #  loop moving on the dics containing all data
+        # if dic contain language if language is not dic so language count=1
+        # f language is in dic so language count will increment
     with open("task6.json","w+") as file:
         json.dump(dic,file,indent=4)  
     return dic
-print(analysis_movie_language())
+print(analysis_movies_language())
