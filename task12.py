cb303e5e452332acd048383700006f181726259d
nehaprajapati123 <91823695+nehaprajapati123@users.noreply.github.com>
2022-01-17 10:10:42 +0530

    Update task12.py

--- task12.py
+++ task12.py
@@ -1,32 +1,34 @@
-
 from bs4 import BeautifulSoup
 import requests
 import json
 import pprint
 url=("https://www.rottentomatoes.com/m/the_lego_movie")
+# whatever movie link will given that's cast we are scrapping.
 movie_link=requests.get(url)
+# requesting link data
 soup=BeautifulSoup(movie_link.text,'html.parser')
-# requests link of a movie and convert it in text.
+#convert it in text.
 div=soup.find('div',class_="castSection")
+# scrapping cast section.
 def scrape_movie_cast(div):
-    # scrap the section where we get the cast.
     list=[]
     for i in div:
         a=i.text
         b=a.split()
-        # print(b)
+        # print(b) names in list
+        # print(b) loop in the data(div) convert it in text form(.text), split(.split) it for removal of white spaces.
         dic={}
         for j in b:
-            # print(j)
+            # pprint.pprint(j)
             c=b[0]+" "+b[1]
-            # print(c)
+            # print(c) we want only 1&2 element of b.
             dic["cast name"]=c
         list.append(dic)
-    # print(list)
+    # print(list) store names in dic and append dics in list.
     for k in list:
         if k=={}:
             list.remove(k)
-        # print(list)
+        # print(list) remove empty dics from the data in list.
     with open("task12.json","w+") as file:
         json.dump(list,file,indent=4)
     return list
